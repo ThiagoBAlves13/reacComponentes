50c3b6140b51eb5fef263155040e5b6e8c9cb130
Thiago Barreto Alves <thiagobalves13@gmail.com>
2025-08-07 15:26:33 -0300

    aula 01 a 04

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,18 @@
 import './colaborador.css'
+import { AiFillCloseCircle } from 'react-icons/ai'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+
+    function deletarColaborador(){
+       aoDeletar(colaborador.id)
+    }
+
     return (<div className="colaborador">
+        <AiFillCloseCircle 
+            size={25} 
+            className="deletar" 
+            onClick={deletarColaborador}
+        />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
